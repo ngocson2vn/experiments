20df44980bbb213389968287ef8ded99948087e1
Son Nguyen <ngocson2vn@gmail.com>
2018-11-08 15:26:57 +0900

    Java Threading

---
+++ golang/Multithread.java
@@ -0,0 +1,28 @@
+// Java code for thread creation by extending 
+// the Thread class 
+class MultithreadingDemo extends Thread { 
+	private String threadName;
+	public MultithreadingDemo(String threadName) {
+		this.threadName = threadName;
+	}
+
+	public void run() { 
+		try {
+			System.out.println(threadName);
+			Thread.sleep(3600000);
+		} catch (Exception e) { 
+			System.out.println ("Exception is caught"); 
+		} 
+	} 
+} 
+
+// Main Class 
+public class Multithread { 
+	public static void main(String[] args) {
+		int n = 1000000;
+		for (int i = 0; i < n; i++) {
+			MultithreadingDemo t = new MultithreadingDemo(String.format("Thread #%d", i));
+			t.start(); 
+		} 
+	} 
+} 
